54c6caca26823a0bc3b80e34ffba9d4050d78a0f
Wonji Kang <wjkang@fadutec.com>
2018-05-10 10:57:26 +0900

    create scenario list view

---
+++ src/views/FioScenarios.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fio-scenarios">
+    <b-container>
+      <b-alert
+        :show="!!errorMessage"
+        variant="danger">
+        {{ errorMessage }}
+      </b-alert>
+      <router-view/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FioScenarios",
+  computed: {
+    errorMessage() {
+      return this.$store.getters.errorMessage;
+    }
+  }
+};
+</script>
---
+++ src/views/FioScenarios/FioScenarioNew.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <b-container>
+      <h1> Add FIO Scenario </h1>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: "FioScenarioNew",
+};
+</script>
